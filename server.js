92529804b92818e2b51e6380b99fd12313e7866c
Jonathan Martins <jonathanmprz@gmail.com>
2026-07-19 12:56:40 -0400

    feat: estrutura inicial do servidor para hospedagem

---
+++ server.js
@@ -0,0 +1,10 @@
+﻿const http = require('http');
+const PORT = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.end('Servidor rodando com sucesso na nuvem!');
+});
+server.listen(PORT, () => {
+  console.log(Servidor rodando com sucesso na porta \!);
+});
